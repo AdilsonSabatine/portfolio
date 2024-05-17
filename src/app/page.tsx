b97c6cb9cc5d336e6fe8f0f0065597d61117ec7e
AdilsonSabatine <adilsonsabatinejunior@gmail.com>
2024-05-17 17:13:59 -0300

    feat: add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./components/Hero"
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] mx-auto container">
-      <div className="container px-12 py-4">
+      <Navbar />
+      <div className="container mt-24 px-12 py-4">
         <Hero />
       </div>
     </main>
