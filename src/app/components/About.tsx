aab769eae677ae9ef5746473a147d65216594743
AdilsonSabatine <adilsonsabatinejunior@gmail.com>
2024-05-19 15:50:45 -0300

    feat: add info to about me

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -3,6 +3,39 @@ import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
 
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>Javascript</li>
+                <li>Java Spring Boot</li>
+            </ul>
+        ),
+    },
+    {
+        title: "Education",
+        id: "education",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>ETEC Hortolandia - IT - Associate</li>
+                <li>FATEC Hortolandia - IT Management - Technology</li>
+            </ul>
+        ),
+    },
+    {
+        title: "Experience",
+        id: "experience",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>Backend Development</li>
+                <li>Chatbots 🤖</li>
+            </ul>
+        ),
+    },
+]
+
 const About = () => {
     const [tab, setTab] = useState("skills");
     const [isPending, startTransition] = useTransition();
@@ -22,16 +55,19 @@ const About = () => {
                     width={500}
                     height={500}
                 />
-                <div>
+                <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
                     <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
                     <p className="text-base lg:text-lg">
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum rerum qui sapiente labore esse, tempora culpa consectetur quo quaerat suscipit quibusdam! Blanditiis molestiae ducimus ea dolore, culpa obcaecati similique impedit.
                     </p>
-                </div>
-                <div className="flex flex-row mt-8">
-                    <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>Skills</TabButton>
-                    <TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>Education</TabButton>
-                    <TabButton selectTab={() => handleTabChange("experience")} active={tab === "experience"}>Experience</TabButton>
+                    <div className="flex flex-row mt-8">
+                        <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>Skills</TabButton>
+                        <TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>Education</TabButton>
+                        <TabButton selectTab={() => handleTabChange("experience")} active={tab === "experience"}>Experience</TabButton>
+                    </div>
+                    <div className="mt-8">
+                        {TAB_DATA.find((t) => t.id === tab).content}
+                    </div>
                 </div>
             </div>
         </section>
