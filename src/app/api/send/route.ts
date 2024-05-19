0e4c95b625319e5df5ef83a28ff9909747eac910
AdilsonSabatine <adilsonsabatinejunior@gmail.com>
2024-05-19 20:35:27 -0300

    feat: add send email functionality

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -10,15 +10,9 @@ export async function POST(req, res) {
     try {
         const data = await resend.emails.send({
             from: fromEmail,
-            to: [fromEmail, email],
+            to: ["adilsonsabatinejunior@gmail.com", email],
             subject: subject,
-            react: `
-            <>
-                <h1> ${subject} </h1>
-                <p>Thank you for contacting us!</p>
-                <p>New message submitted: </p>
-                <p> ${message} </p>
-            < />`,
+            react: `New message submitted: ${message}`,
         });
         return NextResponse.json(data);
     } catch (error) {
--- src/app/components/Email.tsx
+++ src/app/components/Email.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+"use client"
 import GithubIcon from "../../../public/svgs/github-icon.svg";
 import LinkedinIcon from "../../../public/svgs/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
 
 const Email = () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            email: e.target.email.value,
+            subject: e.target.subject.value,
+            message: e.target.message.value,
+        }
+        const endpoint = "/api/send";
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        }
+
+        const response = await fetch(endpoint, options);
+        const resData = await response.json();
+        console.log(resData);
+        if (response.status === 200) {
+            console.log('Message sent')
+        }
+    }
+
     return (
         <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-6 relative">
             <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-red-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2">
@@ -12,8 +36,8 @@ const Email = () => {
             <div className="z-10">
                 <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
                 <p className="text-[#ADB7BE] mb-4 max-w-md">
-                I&apos;m currently looking for new opportunities, my inbox is always open.
-                Wheter you have a question or just want to say hi, I&apos;ll try my best to get back to you!
+                    I&apos;m currently looking for new opportunities, my inbox is always open.
+                    Wheter you have a question or just want to say hi, I&apos;ll try my best to get back to you!
                 </p>
                 <div className="socials flex flex-row gap-2">
                     <Link href={"https://github.com/AdilsonSabatine"} target="/">
@@ -26,11 +50,12 @@ const Email = () => {
             </div>
 
             <div>
-                <form className="flex flex-col" action="">
+                <form className="flex flex-col" onSubmit={handleSubmit}>
                     <div className="mb-6">
                         <label htmlFor="email" className="text-white block mb-2 text-sm font-medium">Your email</label>
                         <input
                             type="text"
+                            name="email"
                             id="email"
                             required
                             className="bg-[#18191E] border-[#33352F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
@@ -41,6 +66,7 @@ const Email = () => {
                         <label htmlFor="subject" className="text-white block mb-2 text-sm font-medium">Subject</label>
                         <input
                             type="text"
+                            name="subject"
                             id="subject"
                             required
                             className="bg-[#18191E] border-[#33352F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
