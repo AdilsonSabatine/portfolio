98b5cf6417e9d46f1119a8c907679559a8074111
AdilsonSabatine <adilsonsabatinejunior@gmail.com>
2024-05-20 00:39:49 -0300

    style: add gradient to header

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -10,8 +10,8 @@ const Hero = () => {
         <section>
             <div className="grid grid-cols-1 sm:grid-cols-12">
                 <div className="col-span-7 place-self-center text-center sm-text-left">
-                    <h1 className="text-white mb-4 text-4xl sm-text-5xl lg:text-6xl font-extrabold">
-                        Olá, sou { }
+                    <h1 className="mb-4 text-4xl sm-text-5xl lg:text-6xl font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4c9ae7] to-[#a5ccf3]">Olá, sou { }</span>
                         <br />
                         <TypeAnimation
                             sequence={[
